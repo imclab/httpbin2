0cd8e7a843165bc9cfb655861f494fce342433bf
Kenneth Reitz <me@kennethreitz.com>
2012-11-29 08:55:36 -0800

    reverse proxy in place!

--- httpbin/main.py
+++ httpbin/main.py
@@ -1,17 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from werkzeug.wsgi import responder
-from werkzeug.utils import redirect
+import os
 
-LEGACY_HTTPBIN = 'httpbin.org'
+import requests
+from flask import Flask, request, Response
+# from werkzeug.wsgi import responder
+# from werkzeug.utils import redirect
+
+LEGACY_HTTPBIN = os.environ.get('LEGACY_HTTPBIN', 'httpbin.org')
+
+
+def convert_response(r):
+    """Creates a Flask Response object from a Requests Response object."""
+
+    return Response(r.content, status=r.status_code, headers=r.headers)
+
 
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def hello():
     return 'Examples....'
 
-@responder
-def fallback(*args):
-    return redirect('http://httpbin.org')
+@app.errorhandler(404)
+def legacy_proxy(e):
+
+    url = 'http://{domain}/{path}'.format(
+        domain=LEGACY_HTTPBIN,
+        path=request.environ['RAW_URI']
+    )
+
+    headers = dict(request.headers.to_list())
+    del headers['Host']
+
+    r = requests.get(url, headers=headers)
+    return convert_response(r)
